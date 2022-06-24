d3001a7267c6a9b077875e68b38d5abc0d7ddf37
coimbra574 <37220954+coimbra574@users.noreply.github.com>
2022-06-24 15:59:10 -0300

    "Added manual seed"

--- main.py
+++ main.py
@@ -57,7 +57,8 @@ if __name__=="__main__":
         opt.experiment = 'samples'
     os.system('mkdir {0}'.format(opt.experiment))
 
-    opt.manualSeed = random.randint(1, 10000) # fix seed
+    #opt.manualSeed = random.randint(1, 10000) # fix seed
+    opt.manualSeed = 1024
     print("Random Seed: ", opt.manualSeed)
     random.seed(opt.manualSeed)
     torch.manual_seed(opt.manualSeed)
