87bd6e93121910c33c85620527ae0a460e123763
coimbra574 <tainacoimbra1996@gmail.com>
2022-06-29 18:20:33 -0300

    Added setup.py in WGAN folder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="models",
+    packages=find_packages("models"),
+    version="0.1.0",
+    description="score_sde package from NVlabs repo",
+    author="NVlabs",
+    license="MIT",
+)
+
